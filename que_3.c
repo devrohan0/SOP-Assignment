a0da5b9c3796f0fa29740995e517f45122570f6e
devrohan0 <omegaaaa07@gmail.com>
2023-03-15 15:55:55 +0530

    Question 3

---
+++ que_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    for(int i = 1; i <= 4; i++) {
+        for(int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+/* output
+1 
+1 2 
+1 2 3 
+1 2 3 4 */
